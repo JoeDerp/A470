9e9422b0c6b77bce90961525e31c4ca69292bd67
Noah Fabrick <nfabrick157@gmail.com>
2024-01-24 11:45:40 -0800

    test

--- Noah_Fabrick_ICA3.py
+++ Noah_Fabrick_ICA3.py
@@ -1,2 +1,13 @@
 # By: Noah Fabrick
 
+class courses:
+    def __init__(self,subject,number,name):
+        self.subject = subject
+        self.number = number
+        self.name = name
+
+c1 = courses('AERO',470,'Model-Based Systems Engineering')
+
+class GEs(courses):
+    pass
+
