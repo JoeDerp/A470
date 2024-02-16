d7c1d44eba3dbabae58ced946d04cec015053cba
Noah Fabrick <nfabrick157@gmail.com>
2024-02-16 00:18:41 -0800

    mbfa complete

--- HW/MBFA_GA_Fabrick.py
+++ HW/MBFA_GA_Fabrick.py
@@ -1,5 +1,64 @@
 # HW2 : Modified Brute-Force and Generic Algorithms
 # By : Noah Fabrick
 
+import random
+
 values = [23, 21, 8, 1, 3, 7, 18, 19, 17, 15, 24, 22, 6, 28, 4, 2, 27, 20, 5, 10]
 weights = [7, 2, 6, 9, 1, 5, 6, 1, 3, 4, 7, 9, 3, 7, 3, 4, 5, 1, 5, 4]
+pairs = []
+for i in range(len(values)):
+    pairs.append((weights[i],values[i]))
+
+
+# MBFA
+def MBFA_knapsack(pairs, maxWeight):
+    pairsCopy = pairs.copy()
+    knapsack = []
+    ksWeight = 0
+    ksValue = 0
+    attempts = 0
+    while True:
+        attempts += 1
+        i = random.randint(0,len(pairsCopy)-1)
+        if pairsCopy[i][0] + ksWeight <= maxWeight:
+            attempts = 0
+            knapsack.append(pairsCopy[i])
+            ksWeight += pairsCopy[i][0]
+            ksValue += pairsCopy[i][1]
+            pairsCopy.pop(i)
+        elif ksWeight == maxWeight:
+            break
+        elif attempts < len(pairsCopy)+1:
+            pass
+        else:
+            break
+
+    return knapsack, ksWeight, ksValue
+
+maxWeight = 45
+N = 0
+ksInvs = []
+ksWeights = []
+ksValues = []
+while N < 50:
+    N += 1
+    knapsack_i, ksWeight_i, ksValue_i = MBFA_knapsack(pairs, 45)
+    ksInvs.append(knapsack_i)
+    ksWeights.append(ksWeight_i)
+    ksValues.append(ksValue_i)
+print('Modified Brute Forrce Algorithm Results after 50 attempts:')
+best1 = ksValues.index(max(ksValues))
+print('1st Best Weight:',ksWeights[best1],'  1st Best Value:',ksValues[best1])
+print('1st Best Inventory:',ksInvs[best1])
+ksWeights.pop(best1)
+ksValues.pop(best1)
+ksInvs.pop(best1)
+best2 = ksValues.index(max(ksValues))
+print('2nd Best Weight:',ksWeights[best2],'  2nd Best Value:',ksValues[best2])
+print('2nd Best Inventory:',ksInvs[best2])
+ksWeights.pop(best2)
+ksValues.pop(best2)
+ksInvs.pop(best2)
+best3 = ksValues.index(max(ksValues))
+print('3rd Best Weight:',ksWeights[best3],'  3rd Best Value:',ksValues[best3])
+print('3rd Best Inventory:',ksInvs[best3])
--- test.py
+++ test.py
@@ -1,52 +1,6 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import random
 
-# Generate random data
-h = np.sqrt(np.random.rand(10000))
-
-# Calculate unit_price, variable_costs, fixed_costs, and unit_sales
-unit_price = (70 - 50) * h * np.random.rand(10000) + 55 - (55 - 50) * h
-variable_costs = (65000 - 50000) * h * np.random.rand(10000) + 55200 - (55200 - 50000) * h
-fixed_costs = (20000 - 10000) * h * np.random.rand(10000) + 14000 - (14000 - 10000) * h
-unit_sales = (3000 - 2000) * h * np.random.rand(10000) + 2440 - (2440 - 2000) * h
-
-# Calculate base earnings
-earnings_base = unit_price * unit_sales - (variable_costs + fixed_costs)
-
-# Perform sensitivity analysis by varying one parameter at a time
-param_names = ['unit_price', 'variable_costs', 'fixed_costs', 'unit_sales']
-param_variations = [np.linspace(0.8, 1.2, 5), np.linspace(0.8, 1.2, 5), np.linspace(0.8, 1.2, 5), np.linspace(0.8, 1.2, 5)]
-
-# Plot sensitivity analysis results with confidence interval
-sensitivity_results = []
-
-plt.figure(figsize=(12, 8))
-
-for i, param_name in enumerate(param_names):
-    for variation in param_variations[i]:
-        if i == 0:
-            unit_price_variation = unit_price * variation
-            earnings_variation = unit_price_variation * unit_sales - (variable_costs + fixed_costs)
-        elif i == 1:
-            variable_costs_variation = variable_costs * variation
-            earnings_variation = unit_price * unit_sales - (variable_costs_variation + fixed_costs)
-        elif i == 2:
-            fixed_costs_variation = fixed_costs * variation
-            earnings_variation = unit_price * unit_sales - (variable_costs + fixed_costs_variation)
-        elif i == 3:
-            unit_sales_variation = unit_sales * variation
-            earnings_variation = unit_price * unit_sales_variation - (variable_costs + fixed_costs)
-
-        sensitivity_results.append(np.mean(earnings_variation))
-
-# Calculate confidence interval for the entire simulation
-confidence_interval = 1.96 * np.std(sensitivity_results) / np.sqrt(len(sensitivity_results))
-
-# Plot the results
-plt.errorbar(np.arange(len(sensitivity_results)), sensitivity_results, yerr=confidence_interval,
-             fmt='o', label='Conf. Int.')
-plt.title('Sensitivity Analysis for the Entire Simulation')
-plt.xlabel('Simulation Samples')
-plt.ylabel('Mean Earnings')
-plt.legend()
-plt.show()
+test = [0, 1, 2, 3]
+x = random.randint(0,len(test)-1)
+print(x)
+print(test[x])
