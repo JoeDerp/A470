abdbbcd072bc1c80dbc64c5544b52ebc60c1a5f8
Noah Fabrick <nfabrick157@gmail.com>
2024-02-24 17:25:35 -0800

    begin biomimetics

---
+++ HW/ACO_Fabrick.py
@@ -0,0 +1,12 @@
+# Ant Colony Optimization applied to Travelling Sales-Ant Problem
+# By: Noah Fabrick
+
+import numpy as np
+
+tau = np.ones((5,5))
+d = np.array([[0,10,12,11,14],[10,0,13,15,8],[12,13,0,9,14],[11,15,9,0,16],[14,8,14,16,0]])
+
+print(tau)
+print(d)
+
+
---
+++ HW/PSO_Ackley.py
@@ -0,0 +1,2 @@
+# Particle Swarm Optimization to solve Ackley Function
+# By: Noah Fabrick
