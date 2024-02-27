4be8f8fab714b2e9466b0d70d7d74e4131038b5a
Noah Fabrick <nfabrick157@gmail.com>
2024-02-26 21:25:06 -0800

    vismat

--- HW/Biomimetics/ACO_Fabrick.py
+++ HW/Biomimetics/ACO_Fabrick.py
@@ -3,10 +3,28 @@
 
 import numpy as np
 
+# Step 1 - Initialization
 tau = np.ones((5,5))
 d = np.array([[0,10,12,11,14],[10,0,13,15,8],[12,13,0,9,14],[11,15,9,0,16],[14,8,14,16,0]])
 
-print(tau)
-print(d)
+# Step 2 - Visibility Matrix and Probabilities
+
+# Updaate Pheremone Matrix
 
 
+
+def visMat(d,r):
+    eta = d.astype(float)
+    for i in range(len(eta)):
+        eta[i][r-1] = 0
+        for j in range(len(eta[0])):
+            if j != r-1:
+                if eta[i][j] != 0:
+                    eta[i][j] = 1/eta[i][j]
+    return eta
+
+def probability(tau,eta,r):
+    pass
+
+
+print(visMat(d,1))
--- test.py
+++ test.py
@@ -1,16 +1,3 @@
-def selection(self, ratio):
-    # Implement Selection
-    # Step 1 - Sort members by fitness
-    self.sort()
-    # Step 2 - return some number of members based on the ratio provided
-    selection = []
-    for i in range(int(len(self)*ratio),len(self)):
-        selection.append(self[i])
-    return selection
-# Example usage:
-
-values = [23, 21, 8, 1, 3, 7, 18, 19, 17, 15, 24, 22, 6, 28, 4, 2, 27, 20, 5, 10]
-
-sorted = values.sort()
-print(values)
-print(selection(values,0.8))
+s = [1, 2, 3, 4, 5]
+s.remove(1)
+print(s)  # Output: [2, 3, 4, 5]
