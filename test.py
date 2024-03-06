10f0d3cffb56eb55e9b71bebbaa5492d6ffcc679
Noah Fabrick <nfabrick157@gmail.com>
2024-03-06 11:17:21 -0800

    githelp

--- test.py
+++ test.py
@@ -2,23 +2,5 @@ from numpy import *
 
 import matplotlib.pyplot as plt
 
-def objective(x, y):
- return -20.0 * exp(-0.2 * sqrt(0.5 * (x**2 + y**2)))-exp(0.5 * (cos(2 * 
-  pi * x)+cos(2 * pi * y))) + e + 20
-
-
-r_min, r_max = -32.768, 32.768
-xaxis = arange(r_min, r_max, 2.0)
-yaxis = arange(r_min, r_max, 2.0)
-x, y = meshgrid(xaxis, yaxis)
-results = objective(x, y)
-figure = plt.figure()
-axis = figure.gca( projection='3d')
-axis.plot_surface(x, y, results, cmap='jet', shade= "false")
-plt.show()
-plt.contour(x,y,results)
-plt.show()
-plt.scatter(x, y, results)
-plt.show()
 
 print('This is a test')
