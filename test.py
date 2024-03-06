efd1924e31ee9c44c4667634fc32422d830c722f
Noah Fabrick <nfabrick157@gmail.com>
2024-03-06 11:30:52 -0800

    final test of git

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from numpy import *
 import matplotlib.pyplot as plt
 
 
-print('This is a test')
+print('This is a final test for local and remote git work')
 plt.plot([i for i in range(10)],[i for i in range(10)])
 plt.xlabel('X')
 plt.show()
